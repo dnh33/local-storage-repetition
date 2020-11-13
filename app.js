2cd8a9cebc5cebd96ef7e6bd5025dd8b31297855
Daniel <danielhjermitslev@protonmail.com>
2020-11-13 11:25:26 +0100

    add darkmode

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ checkbox.addEventListener("change", function (event) {
     /* event.target.checked = checkbox.checked; */
     if (event.target.checked) {
         window.localStorage.setItem("darkmode", "true");
+        document.body.classList.add("darkmode");
     } else {
         window.localStorage.removeItem("darkmode");
+        document.body.classList.remove("darkmode");
     }
-})
+});
+
+if (window.localStorage.getItem("darkmode")) {
+    checkbox.checked = true;
+    document.body.classList.add("darkmode");
+}
